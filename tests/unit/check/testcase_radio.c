277ae8fd23a927d786d24e50844cc97f414fd92e
Jonathan Gramain <jonathan.gramain@gmail.com>
2017-06-02 18:59:07 -0700

    reorganize some data files
    - move lang_dict.bp to examples/ dir
    - move 'radio' test resources into a tests/common subdir

--- tests/unit/check/testcase_radio.c
+++ tests/unit/check/testcase_radio.c
@@ -106,12 +106,12 @@ static void testcase_radio_setup(void)
         info = &radio_source_info[i];
 
         snprintf(filepath, sizeof (filepath),
-                 "tests/radio/radio_%s.bp", radio_sources[i]);
+                 "tests/common/radio/radio_%s.bp", radio_sources[i]);
         ret = bitpunch_load_schema_from_path(filepath, &info->bp);
         assert(0 == ret);
 
         snprintf(filepath, sizeof (filepath),
-                 "tests/radio/radio_%s.bin", radio_sources[i]);
+                 "tests/common/radio/radio_%s.bin", radio_sources[i]);
         ret = bitpunch_load_binary_file_from_path(filepath, &info->bin);
         assert(0 == ret);
 
