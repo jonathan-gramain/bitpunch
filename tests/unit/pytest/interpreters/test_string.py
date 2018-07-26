8734b47ddd7e61b8ad9905173f6871a57d9666ba
Jonathan Gramain <jonathan.gramain@gmail.com>
2018-07-26 00:07:49 -0700

    move string-boundary related tests to test_string.py

--- tests/unit/pytest/interpreters/test_string.py
+++ tests/unit/pytest/interpreters/test_string.py
@@ -9,7 +9,6 @@ import conftest
 # Test string type
 #
 
-expected_string_table = ['Bonjour', 'Hello', 'Guten Tag', 'Hola', 'Privet']
 
 spec_string_table = """
 let cstr = [] byte <> string { @boundary = '\\0'; };
@@ -33,7 +32,7 @@ file {
 }
 """
 
-data_string_table_multi_char_boundary_1 = """
+data_string_table_multi_char_boundary = """
 "Bonjour"
 "--delimiter--"
 "Hello"
@@ -46,25 +45,136 @@ data_string_table_multi_char_boundary_1 = """
 """
 
 
+spec_string_table_dynamic_boundary_1 = """
+file {
+    let cstr = [] byte <> string { @boundary = delimiter; };
+
+    delimiter: [] byte <> string { @boundary = "\\0"; };
+    string_table: [] cstr;
+}
+"""
+
+data_string_table_dynamic_boundary_1 = """
+"<coOldeLimiTer>" 00
+"Bonjour"
+"<coOldeLimiTer>"
+"Hello"
+"<coOldeLimiTer>"
+"Guten Tag"
+"<coOldeLimiTer>"
+"Hola"
+"<coOldeLimiTer>"
+"Privet"
+"""
+
+spec_string_table_conditional_boundary = """
+file {
+    let cstr = [] byte <> string {
+        if (is_multichar == 1) {
+            @boundary = "|---|";
+        } else {
+            @boundary = "|";
+        }
+    };
+
+    is_multichar: byte <> integer { @signed = false; };
+    string_table: [] cstr;
+}
+"""
+
+data_string_table_conditional_boundary_single = """
+00
+"Bonjour|Hello|Guten Tag|Hola|Privet"
+"""
+
+data_string_table_conditional_boundary_multi = """
+01
+"Bonjour|---|Hello|---|Guten Tag|---|Hola|---|Privet"
+"""
+
+spec_file_string_table_dynamic_boundary_2 = """
+
+let u8 = byte <> integer { @signed = false; };
+
+file {
+    let BoundedString = [] byte <> string { @boundary = boundary; };
+
+    boundary_size: u8;
+    boundary: [boundary_size] byte <> string;
+
+    string_table: [] BoundedString;
+}
+
+"""
+
+data_file_string_table_dynamic_boundary_2_1 = """
+# delimiter size
+01
+# delimiter
+00
+# values
+"Bonjour" 00
+"Hello" 00
+"Guten Tag" 00
+"Hola" 00
+"Privet" 00
+"""
+
+data_file_string_table_dynamic_boundary_2_2 = """
+# delimiter size
+02
+# delimiter
+"  "
+# values
+"Bonjour  Hello  Guten Tag  Hola  Privet"
+"""
+
+data_file_string_table_dynamic_boundary_2_3 = """
+# delimiter size
+09
+# delimiter
+"--delim--"
+# values
+"Bonjour--delim--Hello--delim--Guten Tag--delim--Hola--delim--Privet--delim--"
+"""
+
+
 
 @pytest.fixture(
     scope='module',
     params=[{
         'spec': spec_string_table,
         'data': data_string_table_1,
-        'expected': expected_string_table,
     }, {
         'spec': spec_string_table_multi_char_boundary,
-        'data': data_string_table_multi_char_boundary_1,
-        'expected': expected_string_table,
+        'data': data_string_table_multi_char_boundary,
+    }, {
+        'spec': spec_string_table_dynamic_boundary_1,
+        'data': data_string_table_dynamic_boundary_1,
+    }, {
+        'spec': spec_file_string_table_dynamic_boundary_2,
+        'data': data_file_string_table_dynamic_boundary_2_1,
+    }, {
+        'spec': spec_file_string_table_dynamic_boundary_2,
+        'data': data_file_string_table_dynamic_boundary_2_2,
+    }, {
+        'spec': spec_file_string_table_dynamic_boundary_2,
+        'data': data_file_string_table_dynamic_boundary_2_3,
+    }, {
+        'spec': spec_string_table_conditional_boundary,
+        'data': data_string_table_conditional_boundary_single,
+    }, {
+        'spec': spec_string_table_conditional_boundary,
+        'data': data_string_table_conditional_boundary_multi,
     }])
 def params_string(request):
     return conftest.make_testcase(request.param)
 
 def test_string(params_string):
     params = params_string
-    dtree, expected = params['dtree'], params['expected']
+    dtree = params['dtree']
 
-    assert len(dtree.string_table) == len(expected)
+    assert len(dtree.string_table) == 5
     for i, s in enumerate(dtree.string_table):
-        assert model.make_python_object(dtree.string_table[i]) == expected[i]
+        assert model.make_python_object(dtree.string_table) == \
+            ['Bonjour', 'Hello', 'Guten Tag', 'Hola', 'Privet']
