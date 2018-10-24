jonathan-gramain/bitpunch

commit 0566ffee3ad76b99c45d0eefe2f04eeccd54c77c

various cleanups
- merge logic of tracker_reset_track_path() and box_get_end_path()
  into filter backends

- rename function block_get_first_attribute() ->
  filter_get_first_declared_attribute()

- remove some direct checks against filter ast node types, replaced by
  generic checks (e.g. ast_node_is_rexpr_filter())

libbitpunch/src/filters/array_slice.c
@@ -191,7 +191,7 @@ box_get_n_items__slice_generic(struct box *box, int64_t *item_countp,
                                struct browse_state *bst)
 {
     bitpunch_status_t bt_ret;
-    struct track_path end_path;
+    int64_t array_n_items;
     int64_t index_start;
     int64_t index_end;
     int64_t item_count;
@@ -199,16 +199,14 @@ box_get_n_items__slice_generic(struct box *box, int64_t *item_countp,
     DBG_BOX_DUMP(box);
     assert(box->track_path.type == TRACK_PATH_ARRAY_SLICE);
     if (-1 == box->u.array_generic.n_items) {
-        bt_ret = box_get_end_path(box->parent_box, &end_path, bst);
+        bt_ret = box_get_n_items_internal(box->parent_box, &array_n_items, bst);
         if (BITPUNCH_OK != bt_ret) {
             return bt_ret;
         }
         index_start = box->track_path.u.array.index;
         index_end = box->track_path.u.array_slice.index_end;
-        if (-1 == index_end) {
-            index_end = end_path.u.array.index;
-        } else if (index_end > end_path.u.array.index) {
-            index_end = end_path.u.array.index;
+        if (-1 == index_end || index_end > array_n_items) {
+            index_end = array_n_items;
         }
         if (-1 == index_start) {
             index_start = 0;
@@ -452,6 +450,28 @@ tracker_goto_nth_item_with_key__array_slice(
     return bt_ret;
 }
 
+bitpunch_status_t
+tracker_goto_end_path__array_slice(struct tracker *tk,
+                                   struct browse_state *bst)
+{
+    bitpunch_status_t bt_ret;
+    int64_t n_items;
+    int64_t index_start;
+
+    bt_ret = box_get_n_items_internal(tk->box, &n_items, bst);
+    if (BITPUNCH_OK != bt_ret) {
+        return bt_ret;
+    }
+    // start of slice relative to parent array
+    index_start = tk->box->track_path.u.array.index;
+    if (-1 == index_start) {
+        index_start = 0;
+    }
+    // track path from beginning of parent array
+    tk->cur = track_path_from_array_index(index_start + n_items);
+    return BITPUNCH_OK;
+}
+
 static struct filter_instance *
 array_slice_filter_instance_build(struct ast_node_hdl *item)
 {
@@ -485,6 +505,8 @@ array_slice_filter_instance_build(struct ast_node_hdl *item)
         tracker_goto_next_item_with_key__array_slice;
     b_tk->goto_nth_item_with_key =
         tracker_goto_nth_item_with_key__array_slice;
+    b_tk->goto_end_path = tracker_goto_end_path__array_slice;
+    b_tk->goto_nil = tracker_goto_nil__array_generic;
 
     return slice;
 }

libbitpunch/include/core/ast.h
@@ -193,9 +193,6 @@ int64_t
 ast_node_get_min_span_size(const struct ast_node_hdl *node);
 int
 ast_node_is_slack(const struct ast_node_hdl *node);
-struct ast_node_hdl *
-block_get_first_attribute(const struct ast_node_hdl *filter,
-                          const char *attr_name);
 int
 ast_node_is_indexed(const struct ast_node_hdl *node);
 struct ast_node_hdl *

libbitpunch/include/core/browse_internal.h
@@ -86,9 +86,6 @@ struct box_backend {
                                                     int get_left_offset,
                                                     int64_t *max_slack_offsetp,
                                                     struct browse_state *bst);
-    bitpunch_status_t (*get_end_path)(struct box *box,
-                                      struct track_path *end_pathp,
-                                      struct browse_state *bst);
 };
 
 struct tracker_backend {
@@ -120,10 +117,9 @@ struct tracker_backend {
                                                expr_value_t item_key,
                                                int nth_twin,
                                                struct browse_state *bst);
-    bitpunch_status_t (*goto_track_path)(struct tracker *tk,
-                                         struct track_path path,
-                                         struct browse_state *bst);
-    void              (*reset_track_path)(struct tracker *tk);
+    bitpunch_status_t (*goto_end_path)(struct tracker *tk,
+                                       struct browse_state *bst);
+    void              (*goto_nil)(struct tracker *tk);
 };
 
 int
@@ -249,9 +245,6 @@ bitpunch_status_t
 box_set_end_offset(struct box *box, int64_t end_offset,
                    enum box_offset_type type,
                    struct browse_state *bst);
-bitpunch_status_t
-box_get_end_path(struct box *box, struct track_path *end_pathp,
-                 struct browse_state *bst);
 bitpunch_status_t
 box_read_value_internal(struct box *box,
                         expr_value_t *valuep,

libbitpunch/include/core/filter.h
@@ -211,6 +211,10 @@ filter_attach_native_attribute(
     struct ast_node_hdl *filter,
     const char *attr_name, expr_value_t value);
 
+struct ast_node_hdl *
+filter_get_first_declared_attribute(
+    const struct ast_node_hdl *filter,
+    const char *attr_name);
 
 void
 compile_node_backends__filter__filter(struct ast_node_hdl *filter);

libbitpunch/include/filters/array.h
@@ -69,5 +69,10 @@ tracker_get_item_key__indexed_array_internal(
     expr_value_t *keyp,
     int *nth_twinp,
     struct browse_state *bst);
+bitpunch_status_t
+tracker_goto_end_path__array_generic(struct tracker *tk,
+                                     struct browse_state *bst);
+void
+tracker_goto_nil__array_generic(struct tracker *tk);
 
 #endif

libbitpunch/include/filters/array_slice.h
@@ -69,6 +69,9 @@ tracker_goto_next_key_match__array_slice(struct tracker *tk,
                                          expr_value_t index,
                                          struct track_path search_boundary,
                                          struct browse_state *bst);
+bitpunch_status_t
+tracker_goto_end_path__array_slice(struct tracker *tk,
+                                   struct browse_state *bst);
 
 int
 compile_global_nodes__array_slice(struct compile_ctx *ctx);

libbitpunch/src/core/ast.c
@@ -1194,7 +1194,7 @@ resolve_expr_scoped_recur(struct ast_node_hdl *expr,
     struct list_of_visible_refs visible_refs;
 
     while (NULL != cur_scope
-           && AST_NODE_TYPE_COMPOSITE != cur_scope->filter->ndat->type) {
+           && !ast_node_is_filter(cur_scope->filter)) {
         cur_scope = cur_scope->parent_box;
     }
     if (NULL == cur_scope) {
@@ -2476,11 +2476,7 @@ compile_expr_operator_sizeof(
         target = ast_node_get_named_expr_target(
             target->ndat->u.rexpr_op_filter.target);
     }
-    switch (target->ndat->type) {
-    case AST_NODE_TYPE_BYTE:
-    case AST_NODE_TYPE_COMPOSITE:
-    case AST_NODE_TYPE_ARRAY:
-    case AST_NODE_TYPE_BYTE_ARRAY:
+    if (ast_node_is_filter(target)) {
         if (0 != (target->ndat->u.item.flags & ITEMFLAG_IS_SPAN_SIZE_DYNAMIC)) {
             semantic_error(
                 SEMANTIC_LOGLEVEL_ERROR, &expr->loc,
@@ -2496,8 +2492,6 @@ compile_expr_operator_sizeof(
             expr_value_as_integer(target->ndat->u.item.min_span_size);
         expr->ndat = compiled_type;
         return 0;
-    default:
-        break ;
     }
     if (target->ndat->u.rexpr.dpath_type_mask == EXPR_DPATH_TYPE_UNSET ||
         target->ndat->u.rexpr.dpath_type_mask == EXPR_DPATH_TYPE_NONE) {
@@ -3804,8 +3798,7 @@ ast_node_get_min_span_size(const struct ast_node_hdl *node)
     case AST_NODE_TYPE_ARRAY:
     case AST_NODE_TYPE_BYTE:
     case AST_NODE_TYPE_BYTE_ARRAY:
-    // FIXME add REXPR_FILTER when ready
-    // case AST_NODE_TYPE_REXPR_FILTER:
+    case AST_NODE_TYPE_REXPR_FILTER:
         assert(SPAN_SIZE_UNDEF != node->ndat->u.item.min_span_size);
         return node->ndat->u.item.min_span_size;
     default:
@@ -3830,22 +3823,6 @@ ast_node_is_slack(const struct ast_node_hdl *node)
     }
 }
 
-struct ast_node_hdl *
-block_get_first_attribute(const struct ast_node_hdl *filter,
-                          const char *attr_name)
-{
-    struct named_expr *attr_stmt;
-
-    STATEMENT_FOREACH(
-        named_expr, attr_stmt,
-        filter->ndat->u.rexpr_filter.filter_def->block_stmt_list.attribute_list, list) {
-        if (0 == strcmp(attr_stmt->nstmt.name, attr_name)) {
-            return attr_stmt->expr;
-        }
-    }
-    return NULL;
-}
-
 int
 ast_node_is_indexed(const struct ast_node_hdl *node)
 {
@@ -3857,10 +3834,10 @@ ast_node_is_indexed(const struct ast_node_hdl *node)
         array = (struct filter_instance_array *)
             node->ndat->u.rexpr_filter.f_instance;
         target = ast_node_get_as_type(array->item_type);
-        if (AST_NODE_TYPE_COMPOSITE != target->ndat->type) {
+        if (!ast_node_is_filter(target)) {
             return FALSE;
         }
-        return NULL != block_get_first_attribute(target, "@key");
+        return NULL != filter_get_first_declared_attribute(target, "@key");
     default:
         return FALSE;
     }
@@ -3877,13 +3854,13 @@ ast_node_get_key_expr(const struct ast_node_hdl *node)
         array = (struct filter_instance_array *)
             node->ndat->u.rexpr_filter.f_instance;
         target = ast_node_get_as_type(array->item_type);
-        if (AST_NODE_TYPE_COMPOSITE != target->ndat->type) {
+        if (!ast_node_is_filter(target)) {
             return NULL;
         }
         // TODO: multiple or conditional key expressions currently not
         // supported (needs proper support in index cache)
 
-        return block_get_first_attribute(target, "@key");
+        return filter_get_first_declared_attribute(target, "@key");
     default:
         return NULL;
     }

libbitpunch/src/core/browse.c
@@ -1247,7 +1247,7 @@ box_get_scope_box(struct box *box)
 
     scope = box;
     while (NULL != scope
-           && AST_NODE_TYPE_COMPOSITE != scope->filter->ndat->type) {
+           && !ast_node_is_filter(scope->filter)) {
         scope = scope->parent_box;
     }
     return scope;
@@ -1391,29 +1391,14 @@ tracker_reset_item_cache(struct tracker *tk)
 }
 
 static void
-tracker_reset_track_path(struct tracker *tk)
+tracker_goto_nil(struct tracker *tk)
 {
     struct ast_node_hdl *item;
 
     item = tk->box->filter;
-    switch (item->ndat->type) {
-    case AST_NODE_TYPE_COMPOSITE:
-        tk->cur = track_path_from_composite_field(NULL);
-        break ;
-    case AST_NODE_TYPE_ARRAY:
-    case AST_NODE_TYPE_ARRAY_SLICE:
-    case AST_NODE_TYPE_BYTE_ARRAY:
-    case AST_NODE_TYPE_BYTE_SLICE:
-    case AST_NODE_TYPE_SOURCE:
-        tk->cur = track_path_from_array_index(-1);
-        break ;
-    case AST_NODE_TYPE_REXPR_FILTER:
-        if (NULL != item->ndat->u.rexpr_filter.f_instance->b_tk.reset_track_path) {
-            item->ndat->u.rexpr_filter.f_instance->b_tk.reset_track_path(tk);
-        }
-        break ;
-    default:
-        break ;
+    assert(ast_node_is_rexpr_filter(item));
+    if (NULL != item->ndat->u.rexpr_filter.f_instance->b_tk.goto_nil) {
+        item->ndat->u.rexpr_filter.f_instance->b_tk.goto_nil(tk);
     }
 }
 
@@ -1423,7 +1408,7 @@ tracker_set_dangling_internal(struct tracker *tk)
     tracker_reset_item_cache_internal(tk);
     tracker_reset_dpath_internal(tk);
     tk->flags &= ~TRACKER_AT_END;
-    tracker_reset_track_path(tk);
+    tracker_goto_nil(tk);
 }
 
 void
@@ -1442,7 +1427,7 @@ tracker_construct(struct tracker *o_tk,
     box_acquire(box);
     o_tk->item_size = -1;
     o_tk->item_offset = -1;
-    tracker_reset_track_path(o_tk);
+    tracker_goto_nil(o_tk);
 }
 
 static void
@@ -2091,50 +2076,6 @@ box_compute_end_offset_internal(struct box *box,
 }
 
 bitpunch_status_t
-box_get_end_path(struct box *box, struct track_path *end_pathp,
-                 struct browse_state *bst)
-{
-    const struct ast_node_hdl *node;
-    bitpunch_status_t bt_ret;
-    int64_t n_items;
-    int64_t index_start;
-
-    DBG_BOX_DUMP(box);
-    node = box->filter;
-    switch (node->ndat->type) {
-    case AST_NODE_TYPE_ARRAY:
-    case AST_NODE_TYPE_BYTE_ARRAY:
-    case AST_NODE_TYPE_REXPR_FILTER:
-    case AST_NODE_TYPE_SOURCE:
-        bt_ret = box_get_n_items_internal(box, &n_items, bst);
-        if (BITPUNCH_OK != bt_ret) {
-            return bt_ret;
-        }
-        *end_pathp = track_path_from_array_index(n_items);
-        break ;
-    case AST_NODE_TYPE_ARRAY_SLICE:
-    case AST_NODE_TYPE_BYTE_SLICE:
-        bt_ret = box_get_n_items_internal(box, &n_items, bst);
-        if (BITPUNCH_OK != bt_ret) {
-            return bt_ret;
-        }
-        index_start = box->track_path.u.array.index;
-        if (-1 == index_start) {
-            index_start = 0;
-        }
-        *end_pathp = track_path_from_array_index(index_start + n_items);
-        break ;
-    case AST_NODE_TYPE_COMPOSITE:
-        *end_pathp = track_path_from_composite_field(NULL);
-        break ;
-    default:
-        *end_pathp = TRACK_PATH_NONE;
-        break ;
-    }
-    return BITPUNCH_OK;
-}
-
-bitpunch_status_t
 track_box_contents_internal(struct box *box,
                             struct tracker **tkp, struct browse_state *bst)
 {
@@ -2664,8 +2605,16 @@ static bitpunch_status_t
 tracker_goto_end_path(struct tracker *tk,
                       struct browse_state *bst)
 {
+    struct filter_instance *f_instance;
+
     DBG_TRACKER_DUMP(tk);
-    return box_get_end_path(tk->box, &tk->cur, bst);
+    f_instance = tk->box->filter->ndat->u.rexpr_filter.f_instance;
+    if (NULL == f_instance->b_tk.goto_end_path) {
+        return tracker_error(
+            BITPUNCH_NOT_IMPLEMENTED, tk, tk->box->filter, bst,
+            "filter does not implement goto_end_path() tracker backend function");
+    }
+    return f_instance->b_tk.goto_end_path(tk, bst);
 }
 
 static bitpunch_status_t

commit 5c5c7e4237b921bcedad24aca4de74d81ff67090

fix: fix regression in box_get_n_items__slice_generic()
Fix regression introduced in commit 0566ffe (wrong index arithmetic).

libbitpunch/src/filters/array_slice.c
@@ -205,8 +205,9 @@ box_get_n_items__slice_generic(struct box *box, int64_t *item_countp,
         }
         index_start = box->track_path.u.array.index;
         index_end = box->track_path.u.array_slice.index_end;
-        if (-1 == index_end || index_end > array_n_items) {
-            index_end = array_n_items;
+        if (-1 == index_end ||
+            index_end > index_start + array_n_items) {
+            index_end = index_start + array_n_items;
         }
         if (-1 == index_start) {
             index_start = 0;

tests/unit/pytest/features/test_filter.py
@@ -223,6 +223,7 @@ let AsContents = struct {
     data: [] byte;
 
     let ?data_preview = data[..10];
+    let ?data_glimpse = data[10..][4..7];
 };
 
 file {
@@ -246,6 +247,7 @@ let AsContents = struct {
     data: [] byte;
 
     let ?data_preview = data[..10];
+    let ?data_glimpse = data[10..][4..7];
 };
 
 file {
@@ -270,6 +272,7 @@ let AsContents = struct {
     data: [] byte;
 
     let ?data_preview = data[..10];
+    let ?data_glimpse = data[10..][4..7];
 };
 
 file {
@@ -311,7 +314,9 @@ def test_filter_2(params_filter_2):
     assert dtree.blocks[1].n == 18
     assert str(dtree.blocks[1].data) == 'more contents data'
     assert str(dtree.blocks[1]['?data_preview']) == 'more conte'
+    assert str(dtree.blocks[1]['?data_glimpse']) == 'dat'
     assert str(dtree.eval_expr('blocks[1].?data_preview')) == 'more conte'
+    assert str(dtree.eval_expr('blocks[1].?data_glimpse')) == 'dat'
     assert dtree.blocks[0].n == 16
     assert str(dtree.blocks[0].data) == 'as contents data'
     assert model.make_python_object(dtree.blocks[2]) == {
@@ -323,6 +328,7 @@ def test_filter_2(params_filter_2):
         'data': 'even more contents data'
     }
     assert dtree.eval_expr('blocks[1].?data_preview').get_size() == 10
+    assert dtree.eval_expr('blocks[1].?data_glimpse').get_size() == 3
 
     # up one ancestor => input for base64 filter
     daddy = dtree.eval_expr('^blocks[1]')