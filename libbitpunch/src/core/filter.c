cf128f793e153c78f7e341beb6d2c6fa7b5ebca7
Jonathan Gramain <jonathan.gramain@gmail.com>
2019-02-23 19:25:00 -0800

    remove scope filter

--- libbitpunch/src/core/filter.c
+++ libbitpunch/src/core/filter.c
@@ -140,8 +140,6 @@ filter_class_lookup(const char *name)
     return NULL;
 }
 
-void filter_class_declare_scope(void);
-
 void filter_class_declare_data_source(void);
 void filter_class_declare_file(void);
 
@@ -161,8 +159,6 @@ void filter_class_declare_formatted_integer(void);
 void
 filter_class_declare_std(void)
 {
-    filter_class_declare_scope();
-
     filter_class_declare_data_source();
     filter_class_declare_file();
 
